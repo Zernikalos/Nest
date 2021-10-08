26ae849226f6743c2fc5fbfad8a4faba7b2289ee
Aarón <demenus@users.noreply.github.com>
2021-10-08 01:21:32 +0100

    feat: 🎸 Providing a simple interface to the exporter

---
+++ tools/studio/src/components/StudioLoader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <input type="file" id="file" ref="file" v-on:change="handleFileUpload"/>
+
+        <button @click="submitFile">Load file</button>
+    </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+import {parseObj} from "@mrrobotto/exporter"
+
+const handleFileUpload = async (ev) => {
+    const f = ev.target.files[0]
+    const content = await f.arrayBuffer()
+    const decoder = new TextDecoder()
+    const fileContent = decoder.decode(content)
+    const obj3d = parseObj(fileContent)
+    debugger
+    return obj3d
+}
+
+const submitFile = () => {
+    console.log("submit")
+}
+
+export default {
+    name: "StudioLoader",
+    methods: {
+        handleFileUpload,
+        submitFile
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
