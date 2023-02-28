5f1e0fe918e4c86ec0c921317eae8d5f977745a3
Aarón Negrín <demenus.dev@gmail.com>
2023-02-28 17:40:07 +0000

    feat: 🎸 Improve the file exporter window

--- packages/exporter/index.ts
+++ packages/exporter/index.ts
@@ -1,1 +1,1 @@
-export {parseToMrr} from "./src/parseToMrr"
+export {parseToMrr, DEFAULT_PARSE_OPTIONS, DEFAULT_EXPORT_OPTIONS} from "./src/parseToMrr"
--- packages/studio/src/hooks/useFileApi.ts
+++ packages/studio/src/hooks/useFileApi.ts
@@ -1,12 +1,17 @@
 import httpClient from "../toolbox/httpClient"
 
-export async function expose(filePath: string): Promise<number> {
+async function expose(filePath: string): Promise<number> {
     return (await httpClient.post("/files/expose", {path: filePath})).data
 }
 
-export function buildFilePath(exposeId: number, fileName: string) {
+function buildFilePath(exposeId: number, fileName: string) {
     // @ts-ignore
     return `${import.meta.env.VITE_STUDIO_SERVER_HOST}/files/${exposeId}/${fileName}`
+}
+
+export async function getUrlForFile(filePath: string, fileName: string) {
+    const exposeId = await expose(filePath)
+    return buildFilePath(exposeId, fileName)
 }
 
 export async function getFile(exposeId: number, fileName: string) {
