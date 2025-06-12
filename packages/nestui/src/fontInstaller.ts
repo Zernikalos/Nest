a5b84307eea870b67f7d62e3b4e0c1563ea32fd7
Aarón <demenus.dev@gmail.com>
2025-06-12 02:46:39 +0100

    feat: 🎸 Font configurer

---
+++ packages/nestui/src/fontInstaller.ts
@@ -0,0 +1,35 @@
+// Rajdhani
+import "@fontsource/rajdhani/300.css";
+import "@fontsource/rajdhani/400.css"; // Regular
+import "@fontsource/rajdhani/500.css";
+import "@fontsource/rajdhani/700.css"; // Bold
+
+// Fira Code
+import "@fontsource/fira-code/300.css";
+import "@fontsource/fira-code/400.css"; // Regular
+import "@fontsource/fira-code/500.css";
+import "@fontsource/fira-code/700.css"; // Bold
+
+// JetBrains Mono
+import "@fontsource/jetbrains-mono/400.css"; // Regular
+import "@fontsource/jetbrains-mono/700.css"; // Bold
+
+// Source Code Pro
+import "@fontsource/source-code-pro/400.css"; // Regular
+import "@fontsource/source-code-pro/700.css"; // Bold
+
+// Roboto
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css"; // Regular
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css"; // Bold
+
+// Open Sans
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css"; // Regular
+import "@fontsource/open-sans/700.css"; // Bold
+
+// Lato
+import "@fontsource/lato/300.css";
+import "@fontsource/lato/400.css"; // Regular
+import "@fontsource/lato/700.css"; // Bold
