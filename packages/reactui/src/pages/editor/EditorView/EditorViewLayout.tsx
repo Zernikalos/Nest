9e973298b367c88e66a311d0e057900c008e1566
Aarón <demenus.dev@gmail.com>
2025-09-10 23:15:41 +0100

    fix: 🐛 Workaround for fixing screen size overflowing

--- packages/reactui/src/pages/editor/EditorView/EditorViewLayout.tsx
+++ packages/reactui/src/pages/editor/EditorView/EditorViewLayout.tsx
@@ -10,6 +10,7 @@ import { SidebarProvider } from '@/components/ui/sidebar';
 import { EditorViewTopBar } from './EditorViewTopBar';
 import { EditorViewContent } from './EditorViewContent';
 import { useNestEditorContext } from '../providers/NestEditorContext';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 export const EditorViewLayout: React.FC = () => {
     const [activeView, setActiveView] = useState<'form' | 'code' | 'viewer'>('form');
@@ -21,20 +22,20 @@ export const EditorViewLayout: React.FC = () => {
     } = useNestEditorContext();
     
     return (
-        <ResizablePanelGroup direction="horizontal" className="h-full w-full">
-            <ResizablePanel defaultSize={25}>
-                <SidebarProvider className="relative w-full">
+        <ResizablePanelGroup direction="horizontal" className="h-screen w-full">
+            <ResizablePanel defaultSize={25} className="h-screen">
+                <ScrollArea className="relative w-full h-screen">
                     <TreeView
-                        className="w-full"
+                        className="w-full h-full"
                         data={tree}
                         selectedIds={selectedIds}
                         onSelect={handleSelect}
                     />
-                </SidebarProvider>
+                </ScrollArea>
             </ResizablePanel>
             <ResizableHandle />
             <ResizablePanel defaultSize={75}>
-                <div className="flex flex-col h-full">
+                <div className="flex flex-col h-screen">
                     <EditorViewTopBar
                         activeView={activeView}
                         onViewChange={setActiveView}
