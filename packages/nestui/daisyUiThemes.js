de9b57fac30280ddd1b11dcb4b364e0436ccf147
Aarón <demenus.dev@gmail.com>
2025-11-14 01:33:03 +0000

    refactor: 💡 Old vue UI removed

--- packages/nestui/daisyUiThemes.js
+++
@@ -1,4 +0,0 @@
-export const lightThemes = ["light", "cupcake", "retro"]
-export const darkThemes = ["dark", "sunset"]
-
-export default [...lightThemes, ...darkThemes]
--- packages/nestui/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  plugins: [
-      // require('autoprefixer'),
-      // require('tailwindcss/nesting'),
-      // require('tailwindcss')(path.join(__dirname,'tailwind.config.js')),
-  ],
-}
